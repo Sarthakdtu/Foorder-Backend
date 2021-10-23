6d945ccc30dfb749c67f0df7079257976dbc3d44
Sarthak Srivastava <sarthaksrivastavadtu712@gmail.com>
2021-10-24 03:44:28 +0530

    added health check

---
+++ src/main/java/com/foorder/FoorderApplication.java
@@ -0,0 +1,13 @@
+package com.foorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoorderApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/foorder/health/HealthCheck.java
@@ -0,0 +1,20 @@
+package com.foorder.health;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("api/health")
+public class HealthCheck {
+
+    @GetMapping("/")
+    public Object getHealth(){
+        HashMap<String, Boolean> result = new HashMap<>();
+        result.put("alive", true);
+        return result;
+    }
+}
