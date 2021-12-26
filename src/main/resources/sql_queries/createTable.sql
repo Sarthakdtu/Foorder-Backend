e6bcad2ca4e4861257efc3bb00fec67693026cab
Sarthakdtu <sarthaksrivastavadtu712@gmail.com>
2021-12-26 18:37:24 +0530

    docker cmd and create table

--- src/main/resources/sql_queries/createTable.sql
+++ src/main/resources/sql_queries/createTable.sql
@@ -2,3 +2,10 @@
 CREATE TABLE CITY(
     name TEXT PRIMARY KEY NOT NULL
 );
+
+insert into city values('Delhi');
+
+
+CREATE TABLE STREET(name TEXT NOT NULL, cityName TEXT NOT NULL, pincode TEXT NOT NULL, id TEXT, PRIMARY KEY(name, cityName));
+
+INSERT INTO STREET(name, cityName, pincode) values('gandhi marg', 'Delhi', '201301');
