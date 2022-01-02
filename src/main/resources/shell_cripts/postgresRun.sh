2a51086f6624c9469405d7fe0f86d6e3c5168171
Sarthakdtu <sarthaksrivastavadtu712@gmail.com>
2022-01-02 18:15:26 +0530

    scripts psql

--- src/main/resources/shell_cripts/postgresRun.sh
+++ src/main/resources/shell_cripts/postgresRun.sh
@@ -1,2 +1,2 @@
 sudo docker run --rm --name postgresql -p 5432:5432 -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin -e POSTGRES_DB=demodb -d postgres:latest
-sudo docker exec -it postgresql psql -d demodb -U admin
+#sudo docker exec -it postgresql psql -d demodb -U admin
